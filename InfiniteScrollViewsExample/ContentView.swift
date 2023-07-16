f51361d41e064145168ffd33040f6eb0a17dec32
Antoine Bollengier <44288655+b5i@users.noreply.github.com>
2023-07-16 16:49:41 +0200

    Initial Commit

---
+++ InfiniteScrollViewsExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//
+//  Created by Antoine Bollengier (github.com/b5i) on 16.07.2023.
+//  Copyright © 2023 Antoine Bollengier. All rights reserved.
+//  
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InfiniteScrollViewsExample/InfiniteScrollViewsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteScrollViewsExampleApp.swift
+//
+//  Created by Antoine Bollengier (github.com/b5i) on 16.07.2023.
+//  Copyright © 2023 Antoine Bollengier. All rights reserved.
+//  
+
+import SwiftUI
+
+@main
+struct InfiniteScrollViewsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
